2d2a3e9bfe20fde254e011cf096535469e3f5cc4
Brandon El Haj <brandon.elhaj@gmail.com>
2019-02-02 15:19:54 -0800

    Final Shopping List

--- index.js
+++ index.js
@@ -21,4 +21,3 @@ $(function() {
 
 });
 
-// '<li><span class="shopping-item">' + userShoppingItem + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>'
