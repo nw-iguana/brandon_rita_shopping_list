5bf3436bca04a1fc8121a05db757948aae720ec1
Brandon El Haj <brandon.elhaj@gmail.com>
2019-02-02 14:24:54 -0800

    4th commit

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-
 $(function() {
     $('#js-shopping-list-form').on('submit', function(event) {
         event.preventDefault();
-        
+
         const userShoppingItem = $('#shopping-list-entry').val();
         $('.shopping-list').on('click').append('<li><span class="shopping-item">' + userShoppingItem + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
         console.log('userShoppingItem', userShoppingItem);
         $('#shopping-list-entry').val('');
 
     })
-    
-    $(document).on('click', '.shopping-item-toggle', function(event) {
-        // let checkedItem = $(this).find('shopping-item').text();
-        console.log(this, $(this));
 
+    $(document).on('click', '.shopping-item-toggle', function(event) {
+        let itemText = $(this).find('.shopping-item').toggleClass('shopping-item__checked');
     })
-      
+
+    $('li').on('click', function(event) {
+        let itemDelete = $(this).remove('shopping-item');
+    })
+
+
 
 });
 
