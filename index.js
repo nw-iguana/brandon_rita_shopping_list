ad3639404fe9d8a1c808bc2cf10220c68e74e50d
Brandon El Haj <brandon.elhaj@gmail.com>
2019-02-02 12:07:52 -0800

    2nd Commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 $(function() {
-    $('js-shopping-list-form').submit(event => {
+    $('#js-shopping-list-form').submit(event => {
         event.preventDefault();
         
-        const userShoppingItem = $(this).find('shopping-list-entry');
+        const userShoppingItem = $(this).find('#shopping-list-entry');
+
     })
 });
