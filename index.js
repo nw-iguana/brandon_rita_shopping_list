1a0a976f3bbcf7c153cbf767d9935e3e43b1e708
Brandon El Haj <brandon.elhaj@gmail.com>
2019-02-03 16:01:40 -0800

    Working version of shopping list

--- index.js
+++ index.js
@@ -4,20 +4,18 @@ $(function() {
 
         const userShoppingItem = $('#shopping-list-entry').val();
         $('.shopping-list').on('click').append('<li><span class="shopping-item">' + userShoppingItem + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
-        console.log('userShoppingItem', userShoppingItem);
+    
         $('#shopping-list-entry').val('');
 
     })
 
-    $('li').on('click', function(event) {
-        let itemText = $(this).find('.shopping-item').toggleClass('shopping-item__checked');
-    })
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    });
 
-    $('li').on('click', function(event) {
-        $(this).remove('span.shopping-item');
-    })
-
-
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+        $(this).closest('li').remove();
+    });
 
 });
 
